ae9a32a6d8c3a3b860d5f041947b66ff7a530637
Rohan Tumkur <113555309+Rolo454@users.noreply.github.com>
2023-11-10 17:34:34 -0500

    Update header.php

--- bread_shop/header.php
+++ bread_shop/header.php
@@ -1,10 +1,4 @@
-<!--
-Rohan Tumkur
-11/3/2023
-IT202-003
-Section 003 Unit 7 Asignment
-rat6@njit.edu
--->
+
 <header>
     <img src = "images/logo.webp" alt = 'Voyage Logo' height = 75px />
     <h1>Voyage Bakery</h1>
